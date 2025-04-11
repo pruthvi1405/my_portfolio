085f77121d01c0544541a0a1388473e624915519
pruthvi1405 <niranjan.pr@northeastern.edu>
2025-04-11 18:33:14 -0400

    testing1.0

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/my_portfolio/', // 👈 important: use your GitHub repo name here
-  plugins: [vue()]
+  base: '/my_portfolio/',
+  plugins: [vue()],
 })
